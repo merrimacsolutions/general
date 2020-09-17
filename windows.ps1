8c98e7994089f30a76af22bfbb7e0a7dfdae3866
merrimacsolutions <71463010+merrimacsolutions@users.noreply.github.com>
2020-09-17 13:56:52 -0500

    Update windows.ps1
    v2

--- windows.ps1
+++ windows.ps1
@@ -2,7 +2,7 @@
 New-Item -ItemType directory -Path C:\temp\
 
 # Download .zip file with beats already compiled for deployment. (merrimac-internal)
-# Invoke-WebRequest "https://merrimacsolutions.sharefile.com/d-s68b525de6cb4fd2b" -OutFile "C:\temp\modified_uncompressed.zip"
+Invoke-WebRequest "https://merrimacsolutions.sharefile.com/d-s68b525de6cb4fd2b" -OutFile "C:\temp\modified_uncompressed.zip"
 
 # Invoke and use unzip to decompress zip file from previous step.
 Add-Type -AssemblyName System.IO.Compression.FileSystem
