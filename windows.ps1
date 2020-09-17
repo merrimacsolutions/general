25568435a8c7f4a9eb035fba424a4099859cbc4a
merrimacsolutions <71463010+merrimacsolutions@users.noreply.github.com>
2020-09-17 13:58:14 -0500

    Add files via upload

---
+++ windows.ps1
@@ -0,0 +1,50 @@
+# Create Temperary Directory if none exsists.
+New-Item -ItemType directory -Path C:\temp\
+
+# Download .zip file with beats already compiled for deployment. (merrimac-internal)
+# Invoke-WebRequest "https://merrimacsolutions.sharefile.com/d-s68b525de6cb4fd2b" -OutFile "C:\temp\modified_uncompressed.zip"
+
+# Invoke and use unzip to decompress zip file from previous step.
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function unzip {
+	param( [string]$ziparchive, [string]$extractpath )
+	[System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
+}
+
+unzip "C:\temp\modified_uncompressed.zip" "C:\temp\modified_uncompressed"
+
+# Metricbeat installation.
+Set-Location "C:\temp\modified_uncompressed\modified_uncompressed\Metricbeat"
+Copy-Item -Path "C:\temp\modified_uncompressed\modified_uncompressed\Metricbeat\*" -Destination "C:\Program Files\Metricbeat" -Recurse
+Set-Location "C:\Program Files\Metricbeat"
+Invoke-expression -Command ".\install-service-metricbeat.ps1"
+Start-Service metricbeat
+
+# Winlogbeat installation.
+Set-Location "C:\temp\modified_uncompressed\modified_uncompressed\Winlogbeat"
+Copy-Item "C:\temp\modified_uncompressed\modified_uncompressed\Winlogbeat" -Destination "C:\Program Files\Winlogbeat" -Recurse
+Set-Location "C:\Program Files\Winlogbeat"
+Invoke-expression -Command ".\install-service-winlogbeat.ps1"
+Start-Service winlogbeat
+
+# Auditbeat installation.
+Set-Location "C:\temp\modified_uncompressed\modified_uncompressed\Auditbeat"
+Copy-Item "C:\temp\modified_uncompressed\modified_uncompressed\Auditbeat" -Destination "C:\Program Files\Auditbeat" -Recurse
+Set-Location "C:\Program Files\Auditbeat"
+Invoke-expression -Command "C:\Program Files\Auditbeat\.\install-service-Auditbeat.ps1"
+Start-Service auditbeat
+
+# Packetbeat installation
+Invoke-WebRequest "https://nmap.org/npcap/dist/npcap-0.9997.exe" -OutFile "C:\temp\npcap-0.9997.exe"
+Set-Location "C:\temp\"
+# Start-Process .\npcap-0.9997.exe /S -NoNewWindow -Wait -PassThru
+Set-Location "C:\temp\modified_uncompressed\modified_uncompressed\Packetbeat"
+Copy-Item "C:\temp\modified_uncompressed\modified_uncompressed\Packetbeat" -Destination "C:\Program Files\Packetbeat" -Recurse
+Set-Location "C:\Program Files\Packetbeat"
+Invoke-expression -Command "C:\Program Files\Packetbeat\.\install-service-packetbeat.ps1"
+Start-Service packetbeat
+
+# Clean-up and file removal.
+Remove-Item "C:\temp\modified_uncompressed.zip"
+Remove-Item -LiteralPath "C:\temp\modified_uncompressed" -Force -Recurse
+Remove-Item "C:\temp\npcap-0.9997.exe"
